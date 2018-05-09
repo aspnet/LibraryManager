53d7c7a7ca2af574dbf59621cd3c9f7dbbdbb951
Prafull Bhosale <prbhosal@microsoft.com>
2018-05-09 11:36:20 -0700

    Change appveyor build script to use newer dotnet install

---
+++ Install-dotnet.ps1
@@ -0,0 +1,6 @@
+$url="https://raw.githubusercontent.com/dotnet/cli/release/2.1.3xx/scripts/obtain/dotnet-install.ps1"
+$output="$PSScriptRoot\dotnet-install.ps1"
+$installDir = Join-Path $PSScriptRoot ".dotnet"
+
+(New-Object System.Net.WebClient).DownloadFile($url, $output) 
+Invoke-Expression "& `"$output`" -InstallDir $installDir -Channel release/2.1.3xxx -Version 2.1.300-rtm-008823" 
