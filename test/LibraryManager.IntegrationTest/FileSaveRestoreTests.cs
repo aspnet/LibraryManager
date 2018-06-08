6c3e30c5ea347c538f7fa9c2ff89c84f298142d7
Yang Lyu <yalyu@microsoft.com>
2018-06-07 19:06:29 -0700

    Adding file save restore tests

---
+++ test/LibraryManager.IntegrationTest/FileSaveRestoreTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using Microsoft.Test.Apex.VisualStudio;
+using Microsoft.Test.Apex.VisualStudio.Solution;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Web.LibraryManager.IntegrationTest
+{
+    [TestClass]
+    public class FileSaveRestoreTests : VisualStudioLibmanHostTest
+    {
+        ProjectTestExtension _webProject;
+        ProjectItemTestExtension _libManConfig;
+
+        [TestInitialize()]
+        public void initialize()
+        {
+            string projectName = "TestProjectCore20";
+
+            _webProject = Solution.ProjectsRecursive[projectName];
+            _libManConfig = _webProject.Find(SolutionItemFind.FileName, "libman.json");
+        }
+
+        [TestMethod]
+        public void FileSaveRestore_Cdnjs_AddNewLibrariesWithoutSpecifingFiles()
+        {
+            _libManConfig.Open();
+            string[] expectedFilesAndFolders = new[] {
+                "jquery.js",
+            };
+
+            Editor.Caret.MoveDown();
+            Editor.Caret.MoveToEndOfLine();
+            Editor.KeyboardCommands.Enter();
+            Editor.KeyboardCommands.Type("\"defaultProvider\": \"cdnjs\",");
+
+            Editor.Caret.MoveDown(3);
+            Editor.KeyboardCommands.Type("\"library\": \"jquery@3.3.1\",");
+            Editor.KeyboardCommands.Enter();
+            Editor.KeyboardCommands.Type("\"destination\":");
+            Editor.KeyboardCommands.Type("wwwroot/lib/jquery");
+
+            _libManConfig.Save();
+            string cwd = Path.Combine(Path.GetDirectoryName(_webProject.FullPath), "wwwroot", "lib", "jquery");
+            LibmanTestsUtility.WaitForRestoredFiles(cwd, expectedFilesAndFolders, caseInsensitive: true);
+        }
+    }
+}
--- test/LibraryManager.IntegrationTest/LibmanTestsUtility.cs
+++ test/LibraryManager.IntegrationTest/LibmanTestsUtility.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using Microsoft.Test.Apex.Editor;
 using Microsoft.Test.Apex.VisualStudio.Editor;
 using Omni.Common;
@@ -9,6 +9,86 @@ namespace Microsoft.Web.LibraryManager.IntegrationTest
 {
     public class LibmanTestsUtility
     {
+        private static HashSet<string> GetTopLevelDirectoriesAndFiles(string cwd, bool caseInsensitive)
+        {
+            HashSet<string> topLevelItems = caseInsensitive ? new HashSet<string>(StringComparer.OrdinalIgnoreCase) : new HashSet<string>();
+            var dir = new DirectoryInfo(cwd);
+
+            if(dir.Exists)
+            {
+                foreach(FileSystemInfo item in dir.EnumerateDirectories())
+                {
+                    topLevelItems.Add(item.Name);
+                }
+                foreach(FileSystemInfo item in dir.EnumerateFiles())
+                {
+                    topLevelItems.Add(item.Name);
+                }
+            }
+
+            return topLevelItems;
+        }
+
+        public static void WaitForRestoredFiles(string cwd, IEnumerable<string> expectedFilesAndFolders, bool caseInsensitive, int timeout = 10000)
+        {
+            string errorMessage = WaitForRestoredFilesHelper(cwd, expectedFilesAndFolders, caseInsensitive, timeout);
+
+            if (errorMessage != null)
+            {
+                string newErrorMessage = WaitForRestoredFilesHelper(cwd, expectedFilesAndFolders, caseInsensitive, timeout);
+
+                if (newErrorMessage != null)
+                {
+                    errorMessage = String.Concat(errorMessage, "\r\nFailed even when forcing completion with double timeout");
+                }
+                else
+                {
+                    errorMessage = String.Concat(errorMessage, "\r\n*Didn't* fail when forcing completion with double timeout");
+                }
+
+                throw new TimeoutException(errorMessage);
+            }
+        }
+
+        private static string WaitForRestoredFilesHelper(string cwd, IEnumerable<string> expectedFilesAndFolders, bool caseInsensitive, int timeout)
+        {
+            string errorMessage = null;
+
+            WaitFor.TryIsTrue(() =>
+            {
+                try
+                {
+                    HashSet<string> topLevelItems = GetTopLevelDirectoriesAndFiles(cwd, caseInsensitive);
+
+                    if (topLevelItems.Count == 0)
+                    {
+                        errorMessage = "Restore failed for the library.";
+                        return false;
+                    }
+
+                    errorMessage = null;
+                    if (expectedFilesAndFolders != null)
+                    {
+                        foreach(string item in expectedFilesAndFolders)
+                        {
+                            if (!topLevelItems.Contains(item))
+                            {
+                                errorMessage = string.Concat(errorMessage, "\r\nTimed out waiting for: ", item, ".");
+                            }
+                        }
+                    }
+                }
+                catch (Exception exc)
+                {
+                    errorMessage = exc.ToString();
+                }
+
+                return errorMessage == null;
+            }, TimeSpan.FromMilliseconds(timeout), TimeSpan.FromMilliseconds(500));
+
+            return errorMessage;
+        }
+
         public static void WaitForCompletionEntries(IVisualStudioTextEditorTestExtension editor, IEnumerable<string> expectedCompletionEntries, bool caseInsensitive, int timeout = 1000)
         {
             string errorMessage = WaitForCompletionEntriesHelper(editor, expectedCompletionEntries, caseInsensitive, timeout);
