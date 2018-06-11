c9e6ffcac01ac8af50d58debc81ab1118c70deed
bhsubra <bhsubra@microsoft.com>
2018-06-11 12:33:46 -0700

    Addressed code review comment

--- src/LibraryManager.Vsix/Commands/InstallLibraryCommand.cs
+++ src/LibraryManager.Vsix/Commands/InstallLibraryCommand.cs
@@ -77,20 +77,22 @@ namespace Microsoft.Web.LibraryManager.Vsix
         {
             Telemetry.TrackUserTask("installdialogopened");
 
-            ProjectItem item = await VsHelpers.GetSelectedItemAsync();
+            ProjectItem item = await VsHelpers.GetSelectedItemAsync().ConfigureAwait(false);
 
             if (item != null)
             {
                 string target = item.FileNames[1];
 
-                Project project = await VsHelpers.GetProjectOfSelectedItemAsync();
+                Project project = await VsHelpers.GetProjectOfSelectedItemAsync().ConfigureAwait(false);
 
                 if (project != null)
                 {
-                    string rootFolder = await project.GetRootFolderAsync();
+                    string rootFolder = await project.GetRootFolderAsync().ConfigureAwait(false);
 
                     string configFilePath = Path.Combine(rootFolder, Constants.ConfigFileName);
                     IDependencies dependencies = Dependencies.FromConfigFile(configFilePath);
+
+                    await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
 
                     UI.InstallDialog dialog = new UI.InstallDialog(dependencies, _libraryCommandService, configFilePath, target, rootFolder);
 
