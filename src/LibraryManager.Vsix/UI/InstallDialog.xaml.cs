1386882bac2d02c30038c4e95f55fc11ee61f942
bhsubra <bhsubra@microsoft.com>
2018-06-25 08:57:13 -0700

    Do not list all files when include all files option is checked

--- src/LibraryManager.Vsix/UI/InstallDialog.xaml.cs
+++ src/LibraryManager.Vsix/UI/InstallDialog.xaml.cs
@@ -198,6 +198,12 @@ namespace Microsoft.Web.LibraryManager.Vsix.UI
 
         private void InstallButton_Clicked(object sender, RoutedEventArgs e)
         {
+            // When "Include all files" option is checked, we don't want to write out the files to libman.json
+            if (IncludeAllLibraryFilesRb.IsChecked.Value)
+            {
+                ViewModel.SelectedFiles.Clear();
+            }
+
             CloseDialog(true);
         }
     }
