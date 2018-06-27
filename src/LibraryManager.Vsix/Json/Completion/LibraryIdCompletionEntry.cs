aa5156f0f705b5ea79299e842b13874463a3b734
Yang Lyu <yalyu@microsoft.com>
2018-06-26 20:24:37 -0700

    Re-organize

---
+++ src/LibraryManager.Vsix/Json/Completion/LibraryIdCompletionEntry.cs
@@ -0,0 +1,108 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.VisualStudio.Imaging.Interop;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.Web.Editor.Completion;
+using Microsoft.VisualStudio.Text;
+using System.Diagnostics;
+
+namespace Microsoft.Web.LibraryManager.Vsix
+{
+    internal class LibraryIdCompletionEntry : SimpleCompletionEntry
+    {
+        internal int Major { get; private set; }
+
+        internal int Minor { get; private set; }
+
+        internal int Patch { get; private set; }
+
+        internal bool IsValid { get; private set; }
+
+        public LibraryIdCompletionEntry(string displayText, string insertionText, ImageMoniker moniker, IIntellisenseSession session)
+            : base(displayText, insertionText, moniker, session)
+        {
+        }
+
+        public LibraryIdCompletionEntry(string displayText, string insertionText, string description, ImageMoniker moniker, ITrackingSpan span, IIntellisenseSession session, bool versionSpecific = false)
+            : base(displayText, insertionText, description, moniker, span, session)
+        {
+            if (versionSpecific)
+            {
+                SetVersion(insertionText);
+            }
+        }
+
+        protected override int InternalCompareTo(CompletionEntry other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+
+            LibraryIdCompletionEntry otherEntry = other as LibraryIdCompletionEntry;
+
+            if (IsValid)
+            {
+                int result = -Major.CompareTo(otherEntry.Major);
+
+                if (result != 0)
+                {
+                    return result;
+                }
+
+                result = -Minor.CompareTo(otherEntry.Minor);
+
+                if (result != 0)
+                {
+                    return result;
+                }
+
+                result = -Patch.CompareTo(otherEntry.Patch);
+
+                if (result != 0)
+                {
+                    return result;
+                }
+            }
+
+            return base.InternalCompareTo(other);
+        }
+
+        private void SetVersion(string insertionText)
+        {
+            if (!string.IsNullOrEmpty(insertionText))
+            {
+                int atIndex = insertionText.IndexOf('@');
+
+                Debug.Assert(atIndex >= 0);
+
+                string versionText = insertionText.Substring(atIndex + 1);
+                string[] versionParts = versionText.Split('.');
+
+                IsValid = true;
+
+                if (versionParts.Length > 0)
+                {
+                    int major;
+                    int.TryParse(versionParts[0], out major);
+                    Major = major;
+                }
+
+                if (versionParts.Length > 1)
+                {
+                    int minor;
+                    int.TryParse(versionParts[1], out minor);
+                    Minor = minor;
+                }
+
+                if (versionParts.Length > 2)
+                {
+                    int patch;
+                    int.TryParse(versionParts[2], out patch);
+                    Patch = patch;
+                }
+            }
+        }
+    }
+}
--- src/LibraryManager.Vsix/Json/Completion/SimpleCompletionEntry.cs
+++ src/LibraryManager.Vsix/Json/Completion/SimpleCompletionEntry.cs
@@ -4,17 +4,13 @@
 using Microsoft.JSON.Editor.Completion;
 using Microsoft.VisualStudio.Imaging.Interop;
 using Microsoft.VisualStudio.Language.Intellisense;
-using Microsoft.Web.Editor.Completion;
 using System.Windows.Media;
 using Microsoft.VisualStudio.Text;
-using System.Diagnostics;
 
 namespace Microsoft.Web.LibraryManager.Vsix
 {
     internal class SimpleCompletionEntry : JSONCompletionEntry
     {
-        internal VersionItem version;
-
         public SimpleCompletionEntry(string text, ImageSource glyph, IIntellisenseSession session)
             : base(text, "\"" + text + "\"", null, glyph, null, false, session as ICompletionSession)
         {
@@ -30,127 +26,29 @@ namespace Microsoft.Web.LibraryManager.Vsix
         {
         }
 
-        public SimpleCompletionEntry(string displayText, string insertionText, string description, ImageMoniker moniker, IIntellisenseSession session, bool versionSpecific = false)
+        public SimpleCompletionEntry(string displayText, string insertionText, string description, ImageMoniker moniker, IIntellisenseSession session)
             : base(displayText, "\"" + insertionText + "\"", description, null, null, false, session as ICompletionSession)
         {
             SetIconMoniker(moniker);
-
-            if (versionSpecific)
-            {
-                SetVersion(insertionText);
-            }
         }
 
-        public SimpleCompletionEntry(string displayText, string insertionText, string description, ImageMoniker moniker, ITrackingSpan span, IIntellisenseSession session, bool versionSpecific = false)
-           : base(displayText, "\"" + insertionText + "\"", description, null, null, false, session as ICompletionSession)
+        public SimpleCompletionEntry(string displayText, string insertionText, string description, ImageMoniker moniker, ITrackingSpan span, IIntellisenseSession session)
+            : base(displayText, "\"" + insertionText + "\"", description, null, null, false, session as ICompletionSession)
         {
             SetIconMoniker(moniker);
             ApplicableTo = span;
-
-            if (versionSpecific)
-            {
-                SetVersion(insertionText);
-            }
         }
 
-        public SimpleCompletionEntry(string displayText, string insertionText, ImageMoniker moniker, ITrackingSpan span, IIntellisenseSession session, bool versionSpecific = false)
+        public SimpleCompletionEntry(string displayText, string insertionText, ImageMoniker moniker, ITrackingSpan span, IIntellisenseSession session)
          : base(displayText, "\"" + insertionText + "\"", null, null, null, false, session as ICompletionSession)
         {
             SetIconMoniker(moniker);
             ApplicableTo = span;
-
-            if (versionSpecific)
-            {
-                SetVersion(insertionText);
-            }
         }
 
         public override bool IsCommitChar(char typedCharacter)
         {
             return typedCharacter == '/' || typedCharacter == '\\';
-        }
-
-        protected override int InternalCompareTo(CompletionEntry other)
-        {
-            var otherEntry = other as SimpleCompletionEntry;
-
-            if (otherEntry == null)
-            {
-                return 1;
-            }
-
-            if (version.IsValid)
-            {
-                int result = -version.Major.CompareTo(otherEntry.version.Major);
-
-                if (result != 0)
-                {
-                    return result;
-                }
-
-                result = -version.Minor.CompareTo(otherEntry.version.Minor);
-
-                if (result != 0)
-                {
-                    return result;
-                }
-
-                result = -version.Patch.CompareTo(otherEntry.version.Patch);
-
-                if (result != 0)
-                {
-                    return result;
-                }
-            }
-
-            return base.InternalCompareTo(other);
-        }
-
-        private void SetVersion(string insertionText)
-        {
-            if (!string.IsNullOrEmpty(insertionText))
-            {
-                int atIndex = insertionText.IndexOf('@');
-
-                Debug.Assert(atIndex >= 0);
-
-                string versionText = insertionText.Substring(atIndex + 1);
-                string[] versionParts = versionText.Split('.');
-
-                version.IsValid = true;
-
-                if (versionParts.Length > 0)
-                {
-                    int major;
-                    int.TryParse(versionParts[0], out major);
-                    version.Major = major;
-                }
-
-                if (versionParts.Length > 1)
-                {
-                    int minor;
-                    int.TryParse(versionParts[1], out minor);
-                    version.Minor = minor;
-                }
-
-                if (versionParts.Length > 2)
-                {
-                    int patch;
-                    int.TryParse(versionParts[2], out patch);
-                    version.Patch = patch;
-                }
-            }
-        }
-
-        internal struct VersionItem
-        {
-            internal int Major;
-
-            internal int Minor;
-
-            internal int Patch;
-
-            internal bool IsValid;
         }
     }
 }
