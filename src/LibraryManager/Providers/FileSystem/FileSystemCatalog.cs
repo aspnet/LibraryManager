60a420c72deee63a5fea7fcfa3d38a5f32b33341
Yang Lyu <yalyu@microsoft.com>
2018-06-29 10:32:04 -0700

    remove unused namespace

--- src/LibraryManager/Providers/FileSystem/FileSystemCatalog.cs
+++ src/LibraryManager/Providers/FileSystem/FileSystemCatalog.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Web.LibraryManager.Contracts;
