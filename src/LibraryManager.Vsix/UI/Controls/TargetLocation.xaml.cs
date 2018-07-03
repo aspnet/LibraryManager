a68cf7d0f36711e8d7d3cfb0c14f35baa75eb691
bhsubra <bhsubra@microsoft.com>
2018-07-03 07:45:27 -0700

    Updated comment

--- src/LibraryManager.Vsix/UI/Controls/TargetLocation.xaml.cs
+++ src/LibraryManager.Vsix/UI/Controls/TargetLocation.xaml.cs
@@ -180,6 +180,8 @@ namespace Microsoft.Web.LibraryManager.Vsix.UI.Controls
             TextChange textChange = e.Changes.Last();
 
             // We will invoke completion on text insertion and not deletion.
+            // Also, we don't want to invoke completion on dialog load as we pre populate the target
+            // location textbox with name of the folder when dialog is initially loaded.
             if (textChange.AddedLength > 0 && TargetLocationSearchTextBox.CaretIndex > 0)
             {
                 VisualStudio.Shell.ThreadHelper.JoinableTaskFactory.Run(async () =>
