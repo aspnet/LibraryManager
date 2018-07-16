cbb09a66881f5913db5e6d4607db8c06ee68b559
Yang Lyu <yalyu@microsoft.com>
2018-07-16 16:32:28 -0700

    remove unnecessary namespace

--- src/LibraryManager.Vsix/UI/InstallDialog.xaml.cs
+++ src/LibraryManager.Vsix/UI/InstallDialog.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
