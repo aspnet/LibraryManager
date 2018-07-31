54b31929f25718afc777dd2bbc2ac1aec91801e6
Jakub Drgoň <jakub.drgon@bsp.sk>
2018-07-31 20:53:36 +0200

    initial jsDelivr npm implementation

---
+++ src/LibraryManager/Providers/jsDelivr/JsDelivrLibrary.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.Web.LibraryManager.Contracts;
+
+namespace Microsoft.Web.LibraryManager.Providers.jsDelivr
+{
+    internal class JsDelivrLibrary : ILibrary
+    {
+        public string Name { get; set; }
+        public string ProviderId { get; set; }
+        public string Version { get; set; }
+        public IReadOnlyDictionary<string, bool> Files { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
