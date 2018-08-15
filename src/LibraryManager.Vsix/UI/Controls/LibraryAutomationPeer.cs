9bbc0d55ff6823724e139faccaf9ae03a601e737
bhsubra <bhsubra@microsoft.com>
2018-08-15 16:16:28 -0700

    Fix accessibility bugs for 15.9

---
+++ src/LibraryManager.Vsix/UI/Controls/LibraryAutomationPeer.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+
+namespace Microsoft.Web.LibraryManager.Vsix.UI.Controls
+{
+    /// <summary>
+    /// Custom AutomationPeer for Library control
+    /// </summary>
+    internal class LibraryAutomationPeer : UserControlAutomationPeer
+    {
+        public LibraryAutomationPeer(UserControl owner) : base(owner)
+        {
+        }
+
+        protected override AutomationControlType GetAutomationControlTypeCore()
+        {
+            return AutomationControlType.Text;
+        }
+    }
+}
