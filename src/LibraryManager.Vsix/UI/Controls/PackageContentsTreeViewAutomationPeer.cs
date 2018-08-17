ccd2774a6f6a59ceb10dc763b0f240a8a1bd2816
bhsubra <bhsubra@microsoft.com>
2018-08-17 09:24:00 -0700

    Fix accessibility bugs reported by Keros tool for 15.9

---
+++ src/LibraryManager.Vsix/UI/Controls/PackageContentsTreeViewAutomationPeer.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+
+namespace Microsoft.Web.LibraryManager.Vsix.UI.Controls
+{
+    /// <summary>
+    /// Custom AutomationPeer for PackageContentsTreeView control
+    /// </summary>
+    internal class PackageContentsTreeViewAutomationPeer : UserControlAutomationPeer
+    {
+        public PackageContentsTreeViewAutomationPeer(UserControl owner) : base(owner)
+        {
+        }
+
+        protected override AutomationControlType GetAutomationControlTypeCore()
+        {
+            return AutomationControlType.Tree;
+        }
+    }
+}
