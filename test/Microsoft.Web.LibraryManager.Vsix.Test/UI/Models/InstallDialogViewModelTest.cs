3c1ce94d2cd02317356d52017086811534458ac4
Jimmy Lewis <jamlew@microsoft.com>
2018-11-02 18:46:53 -0700

    Correctly serialize new library into text buffer when adding via UI
    PR #326 caused a regression in this serialization that would only occur when the manifest file is open in the text buffer while the UI commits changes.  The serializable data in LibraryInstallationState was moved into the LibraryInstallationStateOnDisk class.  This fix makes sure that this specific code path also uses LibraryInstallationStateOnDisk.

---
+++ test/Microsoft.Web.LibraryManager.Vsix.Test/UI/Models/InstallDialogViewModelTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Web.LibraryManager.Vsix.UI.Models;
+using Microsoft.Web.LibraryManager.Providers.Cdnjs;
+using Newtonsoft.Json;
+using Microsoft.Web.LibraryManager.LibraryNaming;
+
+namespace Microsoft.Web.LibraryManager.Vsix.Test.UI.Models
+{
+    [TestClass]
+    public class InstallDialogViewModelTest
+    {
+        // these settings should be kept in sync with those used in GetLibraryTextToBeInserted to ensure that the expectedObj is
+        // serialized the same as resultString.
+        private readonly JsonSerializerSettings _jsonSettings = new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented,
+            NullValueHandling = NullValueHandling.Ignore,
+        };
+        private Manifest _manifest;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var dependencies = new Mocks.Dependencies(new Mocks.HostInteraction(), new CdnjsProviderFactory());
+            LibraryIdToNameAndVersionConverter.Instance.Reinitialize(dependencies);
+            _manifest = new Manifest(dependencies);
+        }
+
+        [TestMethod]
+        public void GetLibraryTextToBeInserted_BasicProperties()
+        {
+            var installState = new LibraryInstallationState
+            {
+                ProviderId = "cdnjs",
+                Name = "jquery",
+                Version = "3.3.1",
+            };
+
+            string resultString = InstallDialogViewModel.GetLibraryTextToBeInserted(installState, _manifest);
+
+            var expectedObj = new
+            {
+                provider = "cdnjs",
+                library = "jquery@3.3.1",
+            };
+            string expected = JsonConvert.SerializeObject(expectedObj, _jsonSettings);
+
+            Assert.AreEqual(expected, resultString);
+        }
+    }
+}
