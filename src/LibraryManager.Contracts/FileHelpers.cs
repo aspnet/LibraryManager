6075cf13a3ab8dd89a86af9a7eaa271037ef0163
alexgav <alexgav@microsoft.com>
2019-02-05 20:07:22 -0800

    Fixing permissions issue in local IIS webs (#446)
    * Fixing permissions issue in local IIS webs
    
    * Pointing to better documentation source on Windows file copy and move behavior

--- src/LibraryManager.Contracts/FileHelpers.cs
+++ src/LibraryManager.Contracts/FileHelpers.cs
@@ -169,7 +169,10 @@ namespace Microsoft.Web.LibraryManager.Contracts
                         string directoryName = Path.GetDirectoryName(destinationFile);
                         Directory.CreateDirectory(directoryName);
 
-                        File.Move(sourceFile, destinationFile);
+                        // Do not use File.Move, as it causes destination not to inherit security permissions.
+                        // Our IIS projects set ACL to give AppPool user permissions, and it gets inherited by project files.
+                        // See https://support.microsoft.com/en-us/help/310316/how-permissions-are-handled-when-you-copy-and-move-files-and-folders                        File.Copy(sourceFile, destinationFile);
+                        File.Delete(sourceFile);
                     }
                 ).ConfigureAwait(false);
 
