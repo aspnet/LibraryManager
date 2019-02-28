8ea7c5c8e990e870d0c08aff6f5005f04a5543c6
bhsubra <bhsubra@BSVMJan22>
2019-02-28 10:08:54 -0800

    Fixed couple other issues - Toggle selection with spacebar, tab navigation, tab stops

--- src/LibraryManager.Vsix/UI/Controls/PackageContentsTreeView.xaml.cs
+++ src/LibraryManager.Vsix/UI/Controls/PackageContentsTreeView.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Microsoft.Web.LibraryManager.Vsix.UI.Models;
 
 namespace Microsoft.Web.LibraryManager.Vsix.UI.Controls
 {
@@ -15,6 +18,21 @@ namespace Microsoft.Web.LibraryManager.Vsix.UI.Controls
         protected override AutomationPeer OnCreateAutomationPeer()
         {
             return new PackageContentsTreeViewAutomationPeer(this);
+        }
+
+        private void OnPreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                TreeView treeView = (TreeView)sender;
+                PackageItem packageItem = treeView.SelectedItem as PackageItem;
+
+                if (packageItem != null)
+                {
+                    packageItem.IsChecked = !packageItem.IsChecked;
+                    e.Handled = true;
+                }
+            }
         }
     }
 }
