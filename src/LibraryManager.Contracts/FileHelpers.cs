f98fa4c1f9e5640e02cfc21a3d9100327679ff55
Alex Gavrilov <alexgav@microsoft.com>
2019-06-27 21:25:51 -0700

    Fixes file permission issues on Linux and MacOS

--- src/LibraryManager.Contracts/FileHelpers.cs
+++ src/LibraryManager.Contracts/FileHelpers.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -42,6 +43,18 @@ namespace Microsoft.Web.LibraryManager.Contracts
             }
             else
             {
+                string originalTempFileName = tempFileName;
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) || RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    // Temp files created on Linux and MacOS by Path.GetTempFileName() will have 600 permissions.
+                    // We want 664 permissions (read permission for all). So rather than using the orignial temp file,
+                    // we will append suffix to its name, which in practical terms still guarantees a unique temp file but
+                    // will have the default 644 permissions when we write to that file.
+                    // See issue https://github.com/aspnet/LibraryManager/issues/475
+                    tempFileName += ".txt";
+                }
+
                 result = await WriteToFileAsync(tempFileName, sourceStream, cancellationToken).ConfigureAwait(false);
 
                 if (result)
@@ -52,19 +65,29 @@ namespace Microsoft.Web.LibraryManager.Contracts
                 // Clean up temp file if we didn't move it to the desination file successfully
                 if (!result)
                 {
-                    try
-                    {
-                        DeleteFiles(new string[] { tempFileName });
-                    }
-                    catch
-                    {
-                        Debug.Fail($"Could not clean up temporary file {tempFileName}");
-                        // Don't fail the operation if we couldn't clean up temporary file
-                    }
+                    SafeDeleteTempFile(tempFileName);
+                }
+
+                if (tempFileName != originalTempFileName)
+                {
+                    SafeDeleteTempFile(originalTempFileName);
                 }
             }
 
             return result;
+        }
+
+        private static void SafeDeleteTempFile(string tempFileName)
+        {
+            try
+            {
+                DeleteFiles(new string[] { tempFileName });
+            }
+            catch
+            {
+                Debug.Fail($"Could not clean up temporary file {tempFileName}");
+                // Don't fail the operation if we couldn't clean up temporary file
+            }
         }
 
         /// <summary>
