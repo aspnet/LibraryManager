924ede59e76710594b0b76134c5e032c0f4ceb2d
Jimmy Lewis <jimmy.lewis@microsoft.com>
2021-12-16 17:35:43 -0800

    Remove "..." for past tense strings (#653)
    The ellipsis should be used to indicate things which are still in
    progress, not things which have completed.  In most cases, this was used
    in a string like "X started...".
    
    It would be acceptable to use "Starting X..." as that is the present
    progressive tense, but we can also simply remove the ellipsis without
    having to re-translate a bunch of strings.
    
    Addresses #212

--- src/LibraryManager.Vsix/Resources/Text.Designer.cs
+++ src/LibraryManager.Vsix/Resources/Text.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Web.LibraryManager.Vsix.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Text {
@@ -106,7 +106,7 @@ namespace Microsoft.Web.LibraryManager.Vsix.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Clean libraries started....
+        ///   Looks up a localized string similar to Clean libraries started.
         /// </summary>
         public static string CleanLibrariesStarted {
             get {
