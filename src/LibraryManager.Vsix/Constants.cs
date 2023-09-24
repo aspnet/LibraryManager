4030c2302b16648393a9feb1cd53706ea024e6b7
Jimmy Lewis <jimmy.lewis@microsoft.com>
2023-09-24 00:59:52 -0700

    Generalize project system interactions to CPS (#717)
    We can share behavior across all CPS projects for handling files additions/removals.  CPS is much more efficient for bulk edits - they work from the file system automatically, so we don't have to do batched edits on the UI thread in VS. This is something we should take advantage of for as many project types as possible, not limited to DotNetCoreWeb.
    
    Addresses #710

--- src/LibraryManager.Vsix/Constants.cs
+++ src/LibraryManager.Vsix/Constants.cs
@@ -14,6 +14,13 @@ namespace Microsoft.Web.LibraryManager.Vsix
         public const string ErrorCodeLink = "https://github.com/aspnet/LibraryManager/wiki/Error-codes#{0}";
         public const string WAP = "{349C5851-65DF-11DA-9384-00065B846F21}";
         public const string WebsiteProject = "{E24C65DC-7377-472B-9ABA-BC803B73C61A}";
+        /// <summary>
+        /// Project capability for .NET web projects. Used only for UI context rules.
+        /// </summary>
         public const string DotNetCoreWebCapability = "DotNetCoreWeb";
+        /// <summary>
+        /// Project capability for CPS-based projects. Used to determine how to add/remove items to the project.
+        /// </summary>
+        public const string CpsCapability = "CPS";
     }
 }
