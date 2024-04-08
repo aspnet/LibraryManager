7bcab3631e4014c161d8b1e4806a62669c9fdf04
Jimmy Lewis <jimmy.lewis@live.com>
2024-04-08 11:45:12 -0700

    Add fileMappings to the DTOs

---
+++ src/LibraryManager.Contracts/FileMapping.cs
@@ -0,0 +1,30 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+using System.Collections.Generic;
+
+namespace Microsoft.Web.LibraryManager.Contracts
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class FileMapping
+    {
+        /// <summary>
+        /// Root path within the library content for this file mapping entry.
+        /// </summary>
+        public string? Root { get; set; }
+
+        /// <summary>
+        /// Destination folder within the project.
+        /// </summary>
+        public string? Destination { get; set; }
+
+        /// <summary>
+        /// The file patterns to match for this mapping, relative to <see cref="Root"/>.  Accepts glob patterns.
+        /// </summary>
+        public IReadOnlyList<string>? Files { get; set; }
+    }
+}
--- src/LibraryManager/Json/LibraryStateToFileConverter.cs
+++ src/LibraryManager/Json/LibraryStateToFileConverter.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Linq;
 using Microsoft.Web.LibraryManager.Contracts;
 using Microsoft.Web.LibraryManager.LibraryNaming;
 
@@ -33,7 +34,8 @@ namespace Microsoft.Web.LibraryManager.Json
                 IsUsingDefaultProvider = string.IsNullOrEmpty(stateOnDisk.ProviderId),
                 ProviderId = provider,
                 DestinationPath = destination,
-                Files = stateOnDisk.Files
+                Files = stateOnDisk.Files,
+                FileMappings = stateOnDisk.FileMappings.Select(f => new Contracts.FileMapping { Destination = f.Destination, Root = f.Root, Files = f.Files }).ToList(),
             };
 
             (state.Name, state.Version) = LibraryIdToNameAndVersionConverter.Instance.GetLibraryNameAndVersion(stateOnDisk.LibraryId, provider);
@@ -49,13 +51,22 @@ namespace Microsoft.Web.LibraryManager.Json
             }
 
             string provider = string.IsNullOrEmpty(state.ProviderId) ? _defaultProvider : state.ProviderId;
-            return new LibraryInstallationStateOnDisk()
+            var serializeState = new LibraryInstallationStateOnDisk()
             {
                 ProviderId = state.IsUsingDefaultProvider ? null : state.ProviderId,
                 DestinationPath = state.IsUsingDefaultDestination ? null : state.DestinationPath,
                 Files = state.Files,
-                LibraryId = LibraryIdToNameAndVersionConverter.Instance.GetLibraryId(state.Name, state.Version, provider)
+                LibraryId = LibraryIdToNameAndVersionConverter.Instance.GetLibraryId(state.Name, state.Version, provider),
+                FileMappings = state.FileMappings?.Select(f => new FileMapping { Destination = f.Destination, Root = f.Root, Files = f.Files }).ToList(),
             };
+
+            if (serializeState is { FileMappings: { Count: 0} })
+            {
+                // if FileMappings is empty, omit it from serialization
+                serializeState.FileMappings = null;
+            }
+
+            return serializeState;
         }
     }
 }
