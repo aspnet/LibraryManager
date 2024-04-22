ed687cc19d6ac668eb7d408d61b5c07b0bbd771c
Jimmy Lewis <jimmy.lewis@live.com>
2024-04-22 13:12:37 -0700

    Add tests for OperationResult

--- src/LibraryManager.Contracts/Properties/AssemblyInfo.cs
+++ src/LibraryManager.Contracts/Properties/AssemblyInfo.cs
@@ -2,4 +2,5 @@
 using System.Runtime.CompilerServices;
 
 [assembly: NeutralResourcesLanguage("en")]
-[assembly:InternalsVisibleTo("Microsoft.Web.LibraryManager.Test")]
+[assembly: InternalsVisibleTo("Microsoft.Web.LibraryManager.Test")]
+[assembly: InternalsVisibleTo("Microsoft.Web.LibraryManager.Contracts.Test")]
