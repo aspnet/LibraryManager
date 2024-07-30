1e63edee7dc85a40ff027aad6abb3962643003f7
Phil Allen <phillipa@microsoft.com>
2024-07-30 11:54:38 -0700

    Update license to MIT

--- test/LibraryManager.Test/Providers/Unpkg/NpmPackageInfoTest.cs
+++ test/LibraryManager.Test/Providers/Unpkg/NpmPackageInfoTest.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Web.LibraryManager.Providers.Unpkg;
 using Newtonsoft.Json.Linq;
 
--- test/TestGlobalSuppressions.cs
+++ test/TestGlobalSuppressions.cs
@@ -1,5 +1,5 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "VSTHRD200:Use \"Async\" suffix for async methods", Justification = "Not for test method names")]
 
