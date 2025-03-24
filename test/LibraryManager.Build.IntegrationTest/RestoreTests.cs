3e3c2672d9c2d95dff0cde3cf341d5fcc5417274
Jimmy Lewis <jimmy.lewis@live.com>
2025-03-24 01:00:59 -0700

    Add a new project with E2E tests for the MSBuild package
    Like the CLI integration test project, this project consumes the nupkg from the .Build package.  It installs the package in a test project, and then can perform test scenarios.
    
    For now a single test is included.

---
+++ test/LibraryManager.Build.IntegrationTest/RestoreTests.cs
@@ -0,0 +1,45 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Web.LibraryManager.Build.IntegrationTest;
+
+[TestClass]
+public class RestoreTests : BuildTestBase
+{
+    [TestMethod]
+    public async Task Restore_LibraryWithFileMapping_NamedFiles()
+    {
+        string manifest = """
+            {
+                "version": "3.0",
+                "defaultProvider": "jsdelivr",
+                "libraries": [
+                    {
+                        "library": "jquery@3.6.0",
+                        "destination": "wwwroot/lib/jquery",
+                        "fileMappings": [
+                            {
+                                "files": [
+                                    "dist/jquery.min.js",
+                                    "dist/jquery.min.map"
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+            """;
+        await CreateManifestFileAsync(manifest);
+
+        await RunDotnetCommandLineAsync("build", TestProjectDirectory);
+
+        AssertFileExists("wwwroot/lib/jquery/dist/jquery.min.js");
+        AssertFileExists("wwwroot/lib/jquery/dist/jquery.min.map");
+    }
+}
