56f1d82e6db8ea5a77515ce1c7506632e5a3e1c7
Jimmy Lewis <jimmy.lewis@live.com>
2025-04-01 23:55:32 -0700

    fixup failing tests
    - file conflicts are now the full path, since the goal state is fully resolved.
    - added a helper to make it easier to read test output when errors don't match.

---
+++ test/LibraryManager.Test/TestUtilities/AssertExtensions.cs
@@ -0,0 +1,31 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Web.LibraryManager.Contracts;
+
+namespace Microsoft.Web.LibraryManager.Test.TestUtilities;
+
+public static class AssertExtensions
+{
+    public static void ErrorsEqual(this Assert assert, IList<IError> expected, IList<IError> actual)
+    {
+        string BuildString(IList<IError> errors)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (IError error in errors)
+            {
+                stringBuilder.AppendLine($"{error.Code}: {error.Message}");
+            }
+            return stringBuilder.ToString();
+        }
+
+        Assert.AreEqual(BuildString(expected), BuildString(actual));
+    }
+}
